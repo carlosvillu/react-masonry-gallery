29babfa598107e1e4f2dcd17b411e140f12d54ee
carlosvillu <carlosvillu@gmail.com>
2015-07-08 13:56:24 +0200

    Updates

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {MasonryGallery} from '../src';
+import '../src/index.scss';
+
+const images = [
+  'orange-butterfly.jpg', 'butterfly-on-yellow-flower.jpg',
+  'butterfly-on-petal.jpg', 'albino-butterfly.jpg',
+  'blue-butterfly.jpg'].map( filename => `https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/${filename}` );
+
+React.render(<MasonryGallery images={images}/>, document.getElementById('main'));
